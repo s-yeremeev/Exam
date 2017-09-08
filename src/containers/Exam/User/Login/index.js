105fcf3bf36ffe337d669144bb5a44391dd0656e
Sasha Yeremeev <c3pions@gmail.com>
2017-09-08 16:40:29 +0300

    4

--- src/containers/Exam/User/Login/index.js
+++ src/containers/Exam/User/Login/index.js
@@ -1,7 +1,22 @@
 import React from "react";
-import { getItem, setItem } from "../../../helpers/localStorage";
+import { getItem, setItem } from "../../helpers/localStorage";
 import InputComponent from "./Input";
 import "./index.scss";
+
+const inputs = [
+    {
+      inputType: "name",
+      inputId: "#{label}",
+      labelFor: "#{label}",
+      inputText: "Username"      
+    },
+    {
+        inputType: "password",
+        inputId: "#{label}",
+        labelFor: "#{label}",
+        inputText: "Password"    
+    }
+  ]
 
 export default class LoginContainer extends React.PureComponent {
     constructor(props) {
@@ -34,24 +49,23 @@ export default class LoginContainer extends React.PureComponent {
                     <div className="card alt">
                         <div className="toggle"></div>
                         <h1 className="title">Register
-    <div className="close"></div>
+                    <div className="close"></div>
                         </h1>
                         <form>
-                            <div className="input-container">
-                                <input type="#{type}" id="#{label}" required="required" />
-                                <label for="#{label}">Username</label>
-                                <div className="bar"></div>
-                            </div>
-                            <div className="input-container">
-                                <input type="#{type}" id="#{label}" required="required" />
-                                <label for="#{label}">Password</label>
-                                <div className="bar"></div>
-                            </div>
-                            <div className="input-container">
-                                <input type="#{type}" id="#{label}" required="required" />
-                                <label for="#{label}">Repeat Password</label>
-                                <div className="bar"></div>
-                            </div>
+                        {
+                        inputs.map((index, input) => (
+                          <InputComponent
+                          {...input} 
+                            key={index}
+                          />
+                        ))
+                      }
+
+                      <div class="input-container">
+                            <input type="#{type}" id="#{label}" required="required"/>
+                            <label for="#{label}">Repeat Password</label>
+                            <div class="bar"></div>
+                        </div>
                             <div className="button-container">
                                 <button><span>Next</span></button>
                             </div>
